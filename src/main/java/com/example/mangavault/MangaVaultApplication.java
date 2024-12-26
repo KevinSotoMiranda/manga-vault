a584dac79c242bfd354511334c39e229a8aeb313
KevinSotoMiranda <levinsoto5@gmail.com>
2024-12-26 13:19:13 -0700

    Update names from demo to mangavault

--- src/main/java/com/example/mangavault/MangaVaultApplication.java
+++ src/main/java/com/example/mangavault/MangaVaultApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example.mangavault;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class MangaVaultApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(MangaVaultApplication.class, args);
 	}
 
 }
--- src/test/java/com/example/mangavault/MangaVaultApplicationTests.java
+++ src/test/java/com/example/mangavault/MangaVaultApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.example.mangavault;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class MangaVaultApplicationTests {
 
 	@Test
 	void contextLoads() {
