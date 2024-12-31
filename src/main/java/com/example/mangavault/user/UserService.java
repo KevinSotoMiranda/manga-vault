6379edbdd2da441a6e91a1f575b147f39b9128d2
KevinSotoMiranda <levinsoto5@gmail.com>
2024-12-30 18:14:22 -0700

    Replace exceptions with custom exception handler in User

--- src/main/java/com/example/mangavault/user/UserService.java
+++ src/main/java/com/example/mangavault/user/UserService.java
@@ -4,7 +4,10 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+
+import com.example.mangavault.exception.ApiRequestException;
 
 @Service
 public class UserService {
@@ -20,7 +23,7 @@ public class UserService {
         Optional<User> userOptional = this.userRepository.findByUsername(user.getUsername());
 
         if(userOptional.isPresent()) {
-            throw new IllegalStateException("Username has been taken already"); // TODO: Create custom exceptions
+            throw new ApiRequestException("Username has been taken already", HttpStatus.CONFLICT);
         }
 
         return this.userRepository.save(user);            
@@ -34,7 +37,7 @@ public class UserService {
         Optional<User> userOptional = this.userRepository.findById(userId);
 
         if(!userOptional.isPresent()) {
-            throw new IllegalStateException(String.format("User not found with id: %d", userId)); // TODO: Create custom exceptions
+            throw new ApiRequestException(String.format("User not found with id: %d", userId), HttpStatus.NOT_FOUND);
         }
 
         return userOptional.get();
@@ -44,7 +47,7 @@ public class UserService {
         Optional<User> userOptional = this.userRepository.findByUsername(username);
 
         if(!userOptional.isPresent()) {
-            throw new IllegalStateException(String.format("User not found with username: %s", username)); // TODO: Create custom exceptions
+            throw new ApiRequestException(String.format("User not found with username: %s", username), HttpStatus.NOT_FOUND);
         }
 
         return userOptional.get();
@@ -52,8 +55,9 @@ public class UserService {
 
     public User updatedUser(Long userId, User user) {
         Optional<User> userOptional = this.userRepository.findById(userId);
+
         if(!userOptional.isPresent()) {
-            throw new IllegalStateException(String.format("User not found with id: %d", userId)); // TODO: Create custom exceptions
+            throw new ApiRequestException(String.format("User not found with id: %d", userId), HttpStatus.NOT_FOUND); 
         }
 
         User existingUser = userOptional.get();
@@ -67,7 +71,7 @@ public class UserService {
         Optional<User> userOptional = this.userRepository.findById(userId);
 
         if(!userOptional.isPresent()) {
-            throw new IllegalStateException(String.format("User not found with id: %d", userId)); // TODO: Create custom exceptions
+            throw new ApiRequestException(String.format("User not found with id: %d", userId), HttpStatus.NOT_FOUND);
         }
 
         this.userRepository.deleteById(userId);
@@ -75,11 +79,11 @@ public class UserService {
 
     private void validateUsername(String username) {
         if(username == null || username.length() < 6) {
-            throw new IllegalStateException("Username must be at least 6 characters"); // TODO: Create custom exceptions
+            throw new ApiRequestException("Username must be at least 6 characters", HttpStatus.BAD_REQUEST); 
         }
 
         else if(username.length() > 255) {
-            throw new IllegalStateException("Username must be at most 255 characters"); // TODO: Create custom exceptions
+            throw new ApiRequestException("Username must be at most 255 characters", HttpStatus.BAD_REQUEST);
         }
     }
 }
