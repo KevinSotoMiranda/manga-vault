7092741d0f96722768caec100bfd0cff520b4936
KevinSotoMiranda <levinsoto5@gmail.com>
2025-01-07 19:29:16 -0700

    Fix migration file error

--- src/main/resources/db/migration/V1__initial_setup.sql
+++ src/main/resources/db/migration/V1__initial_setup.sql
@@ -21,7 +21,7 @@ CREATE TABLE books(
     series_id INT NOT NULL,
     PRIMARY KEY(book_id),
     UNIQUE(isbn_13),
-    FOREIGN KEY(series_id) REFERENCES series(series_id),
+    FOREIGN KEY(series_id) REFERENCES series(series_id)
 );
 
 CREATE TABLE collections(
