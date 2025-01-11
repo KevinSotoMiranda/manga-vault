19f4386a1996f9a0406c695709730ef5921075fe
KevinSotoMiranda <levinsoto5@gmail.com>
2025-01-10 17:05:34 -0700

    Code refactor for Series

--- src/main/java/com/example/mangavault/series/Series.java
+++ src/main/java/com/example/mangavault/series/Series.java
@@ -2,65 +2,51 @@ package com.example.mangavault.series;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Size;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 
 @Entity
+@Getter
+@Builder
+@NoArgsConstructor
 @Table(name = "series")
 public class Series {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "series_sequence")
     @SequenceGenerator(name = "series_sequence", sequenceName = "series_sequence", allocationSize = 1)
+    @Column(name = "series_id")
     private Long id;
 
     @Size(max = 255, message = "Title must be at most 255 characters")
+    @Column(name = "title")
+    @Setter
     private String title;
-    @Size(max = 255, message = "Author name must be at most 255 characters")
-    private String author;
 
-    public Series() {
-    }
+    @Size(max = 255, message = "Author name must be at most 255 characters")
+    @Column(name = "author")
+    @Setter
+    private String author;
 
     public Series(String title, String author) {
         this.title = title;
         this.author = author;
     } 
 
-    public Series(String title, String author, Long id) {
+    private Series(Long id, String title, String author) {
         this.title = title;
         this.author = author;
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public void setId(Long id) {
         this.id = id;
     }
 
     @Override
     public String toString() {
         return "Series{" +
-                "title='" + title + '\'' +
+                "id=" + id + 
+                ", title='" + title + '\'' +
                 ", author='" + author + '\'' +
-                ", id=" + id +
                 '}';
     }
 }
