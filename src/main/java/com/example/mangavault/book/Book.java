c847f6cee86082fe2e614c3a51a7c1e6859f10db
KevinSotoMiranda <levinsoto5@gmail.com>
2025-01-21 10:59:21 -0700

    Code refactor for Book class

--- src/main/java/com/example/mangavault/book/Book.java
+++ src/main/java/com/example/mangavault/book/Book.java
@@ -2,23 +2,27 @@ package com.example.mangavault.book;
 
 import jakarta.persistence.*;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Getter
 @Builder
+@NoArgsConstructor
 @Table(name = "books")
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "book_sequence")
     @SequenceGenerator(name = "book_sequence", sequenceName = "book_sequence", allocationSize = 1)
     private Long id;
+
     private String title;
+
     private String isbn13;
 
     private int volume;
-    private Long series_id;
 
-    public Book() {
-    }
+    private Long series_id;
 
     public Book(String title, String isbn13, int volume, Long series_id) {
         this.title = title;
@@ -32,46 +36,6 @@ public class Book {
         this.isbn13 = isbn13;
         this.volume = volume;
         this.series_id = series_id;
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getIsbn13() {
-        return isbn13;
-    }
-
-    public int getVolume() {
-        return volume;
-    }
-
-    public Long getSeries_id() {
-        return series_id;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public void setIsbn_13(String isbn13) {
-        this.isbn13 = isbn13;
-    }
-
-    public void setVolume(int volume) {
-        this.volume = volume;
-    }
-
-    public void setSeries_id(Long series_id) {
-        this.series_id = series_id;
-    }
-
-    public void setId(Long id) {
         this.id = id;
     }
 
--- src/test/java/com/example/mangavault/book/BookRepositoryTests.java
+++ src/test/java/com/example/mangavault/book/BookRepositoryTests.java
@@ -1,5 +1,7 @@
 package com.example.mangavault.book;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -7,5 +9,12 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 @DataJpaTest
 @AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 public class BookRepositoryTests {
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Test 
+    public void TEst() {
+        
+    }
     
 }
